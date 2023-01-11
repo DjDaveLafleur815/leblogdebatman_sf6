f12dfe693368b13f73ec5eb55eac7f33727390a6
Daviddu815 <david.dharma815@outlook.fr>
2023-01-11 12:23:10 +0100

    Ajout captcha page inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Recaptcha\RecaptchaValidator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -19,7 +21,8 @@ class RegistrationController extends AbstractController
      *   Contrôleur de la page d'inscription
      */
     #[Route('/creer-un-compte/', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher,
+                             EntityManagerInterface $entityManager, RecaptchaValidator $recaptcha): Response
     {
 
         // Si l'utilisateur est déjà connecté, on le redirige de force sur la page d'accueil du site
@@ -37,29 +40,46 @@ class RegistrationController extends AbstractController
         // Remplissage du formulaire avec les données POST (qui sont $request)
         $form->handleRequest($request);
 
-        // Si le formulaire a bien été envoyé et ne possède pas d'erreur
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encodage du mot de passe
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+        // Si le formulaire a bien été envoyé
+        if ($form->isSubmitted()) {
 
-            // Hydratation de la date d'Inscription du nouvel utilisateur
-            $user->setRegistrationDate( new \DateTime() );
+            // Récupération de la valeur du captcha ( $_POST['g-recaptcha-response'] )
+            $captchaResponse = $request->get('REMOTE_ADDR');
 
-            // Sauvegarde du nouvel utilisateur en BDD
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
+            // Récupération de l'adresse IP de l'utilisateur ( $_SERVER['REMOTE_ADDR'] )
+            $ip = $request->server->get('REMOTE_ADDR');
 
-            // Message flash de succès
-            $this->addFlash('success', 'Votre compte a bien été créé avec succès !');
+            // Si le captcha est null ou s'il est invalide, on ajoute une erreur dans le formulaire
+            if ($captchaResponse == null || !$recaptcha->verify($captchaResponse, $ip) ){
 
-            // Redirection de l'utilisateur vers la page de connexion
-            return $this->redirectToRoute('app_login');
+                $form->addError( new FormError('Veuillez remplir le captcha de sécurité') );
+
+            }
+
+            if ($form->isValid()) {
+
+                // encodage du mot de passe
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+
+                // Hydratation de la date d'Inscription du nouvel utilisateur
+                $user->setRegistrationDate( new \DateTime() );
+
+                // Sauvegarde du nouvel utilisateur en BDD
+                $entityManager->persist($user);
+                $entityManager->flush();
+                // do anything else you need here, like send an email
+
+                // Message flash de succès
+                $this->addFlash('success', 'Votre compte a bien été créé avec succès !');
+
+                // Redirection de l'utilisateur vers la page de connexion
+                return $this->redirectToRoute('app_login');
+            }
         }
 
         return $this->render('registration/register.html.twig', [
